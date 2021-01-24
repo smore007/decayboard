1d2f1395e2baf2dc1b01481d1cd4fe88e0c859bf
smore007 <30681316+smore007@users.noreply.github.com>
2021-01-24 16:11:27 -0500

    Added sql uri

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_heroku import Heroku
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zyxznyonpytfnf:6a05f4e3f7997bd3f4c1f995f7f0fae3d28d4202b8294212fbbc22d1379e737a@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d5a8pr2slh4f23'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 heroku = Heroku(app)
