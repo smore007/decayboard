6165fe5c984e1e9e2cb219bc9dd8c428571dd619
smore007 <30681316+smore007@users.noreply.github.com>
2021-01-24 17:27:35 -0500

    Added posting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zyxznyonpytfnf:6a05f4e3f7997bd3f4c1f995f7f0fae3d28d4202b8294212fbbc22d1379e737a@ec2-52-23-86-208.compute-1.amazonaws.com:5432/d5a8pr2slh4f23'
@@ -8,9 +9,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+def get_posts(parent_id=None):
+    return Post.query.filter(Post.parent_id is parent_id).all()
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=get_posts())
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        p = Post(title=title, body=body)
+
+        db.session.add(p)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,15 @@
-from manage import db, app
+from manage import db
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parent_id = db.Column(db.Integer)
     media_url = db.Column(db.String(255))
+    title = db.Column(db.String(128))
     body = db.Column(db.String(4096))
 
-    def __init__(self, body, media_url=None, parent_id=None):
+    def __init__(self, title=None, body=None, media_url=None, parent_id=None):
+        self.title = title
         self.body = body
         self.media_url = media_url
         self.parent_id = parent_id
