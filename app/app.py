dc299664a2ac38d467f0fb227c2b0e7fb9b6f60b
smore007 <30681316+smore007@users.noreply.github.com>
2021-01-24 16:07:38 -0500

    Added posts

--- app/app.py
+++ app/app.py
@@ -7,9 +7,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 
 
+def get_posts(parent_id=None):
+    from . import db
+    return db.Post.query.filter(db.Post.parent_id is parent_id).all()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=get_posts())
 
 
 if __name__ == '__main__':
